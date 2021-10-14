a16ea3bec736ff39d51ab078444013ac28ec9f56
jprivard <jprivard@fibrenoire.ca>
2021-10-14 19:03:08 -0400

    Adding Entry Page + Face + refactor

--- libs/store/pokedex/src/index.ts
+++ libs/store/pokedex/src/index.ts
@@ -1,4 +1,4 @@
 export * from './lib/store-pokedex.module';
 export * from './lib/pokedex.facade';
-export * from './lib/models/pokemon-summary.interface';
+export * from './lib/interfaces/pokemon-summary.interface';
 export * from './lib/fixtures/pokemon-summary.fixture';
