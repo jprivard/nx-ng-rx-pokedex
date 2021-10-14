3dbfa39f06b1e07d86dd59e2e241a16ae4e1d33a
jprivard <jprivard@fibrenoire.ca>
2021-10-13 21:35:35 -0400

    First implementation of new Component Modules

--- libs/store/pokedex/src/lib/fixtures/pokemon-summary.fixture.ts
+++ libs/store/pokedex/src/lib/fixtures/pokemon-summary.fixture.ts
@@ -1,7 +1,7 @@
 import { PokemonSummary } from '../models/pokemon-summary.interface';
 
 export const pokemonSummary = [
-  { id: 1, name: 'Bulbusaur', sprite: 'http://img/1', types: ['Grass', 'Poison'] },
-  { id: 2, name: 'Ivysaur', sprite: 'http://img/2', types: ['Grass', 'Poison'] },
-  { id: 3, name: 'Venusaur', sprite: 'http://img/3', types: ['Grass', 'Poison'] }
+  { id: 1, name: 'Bulbusaur', url: '/pokemon/bulbusaur', sprite: 'http://img/1', types: ['Grass', 'Poison'] },
+  { id: 2, name: 'Ivysaur', url: '/pokemon/ivysaur', sprite: 'http://img/2', types: ['Grass', 'Poison'] },
+  { id: 3, name: 'Venusaur', url: '/pokemon/venusaur', sprite: 'http://img/3', types: ['Grass', 'Poison'] }
 ] as PokemonSummary[];
--- libs/store/pokedex/src/lib/models/pokemon-summary.interface.ts
+++ libs/store/pokedex/src/lib/models/pokemon-summary.interface.ts
@@ -1,6 +1,7 @@
 export interface PokemonSummary {
   id: number;
   name: string;
+  url: string;
   sprite: string;
   types: any[];
 }
