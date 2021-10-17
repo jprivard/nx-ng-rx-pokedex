ece5632e6005b5f8b99c9e11017c934e9fd24d60
jprivard <jprivard@fibrenoire.ca>
2021-10-17 12:58:08 -0400

    Exporting components

---
+++ libs/web/entry/src/lib/components/index.ts
@@ -0,0 +1,19 @@
+import { AbilitiesComponent } from './abilities/abilities.component';
+import { HeaderComponent } from './header/header.component';
+import { LayoutComponent } from './layout/layout.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { QuoteComponent } from './quote/quote.component';
+import { SpeciesComponent } from './species/species.component';
+import { StatsComponent } from './stats/stats.component';
+import { TypesComponent } from './types/types.component';
+
+export const COMPONENTS = [
+  SpeciesComponent,
+  LayoutComponent,
+  NotFoundComponent,
+  HeaderComponent,
+  QuoteComponent,
+  StatsComponent,
+  TypesComponent,
+  AbilitiesComponent,
+];
